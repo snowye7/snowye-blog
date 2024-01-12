e95d55de477da3e0c8407c1423bbff69752aae4f
snowye <10700876+snowye@user.noreply.gitee.com>
2024-01-13 02:14:25 +0800

    “first commit”

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import siteMetadata from "@/data/siteMetadata"
+import headerNavLinks from "@/data/headerNavLinks"
+import Link from "./Link"
+import MobileNav from "./MobileNav"
+import ThemeSwitch from "./ThemeSwitch"
+import SearchButton from "./SearchButton"
+
+const Header = () => {
+    return (
+        <header className="flex items-center justify-between py-4">
+            <div>
+                <Link href="/" aria-label={siteMetadata.headerTitle}>
+                    <div className="flex items-center justify-between gap-3">
+                        <img src="/static/images/snow-globe.gif" alt="" width={82} />
+                        {typeof siteMetadata.headerTitle === "string" ? <div className="hidden h-6 text-2xl font-semibold sm:block leading-6">{siteMetadata.headerTitle}</div> : siteMetadata.headerTitle}
+                    </div>
+                </Link>
+            </div>
+            <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+                {headerNavLinks
+                    .filter(link => link.href !== "/")
+                    .map(link => (
+                        <Link key={link.title} href={link.href} className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block hover:bg-[rgba(35,39,47,0.1)] rounded-lg px-3 py-2">
+                            {link.title}
+                        </Link>
+                    ))}
+                <SearchButton />
+                <ThemeSwitch />
+                <MobileNav />
+            </div>
+        </header>
+    )
+}
+
+export default Header
