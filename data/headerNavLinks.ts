bbe70e2fdb351464232e705070dfbe5bf22dc344
snowye <10700876+snowye@user.noreply.gitee.com>
2024-01-13 02:57:25 +0800

    "eslint"

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-    { href: "/", title: "主页" },
-    { href: "/blog", title: "博客" },
-    { href: "/tags", title: "标签" },
-    { href: "/about", title: "关于" }
+  { href: '/', title: '主页' },
+  { href: '/blog', title: '博客' },
+  { href: '/tags', title: '标签' },
+  { href: '/about', title: '关于' },
 ]
 
 export default headerNavLinks
