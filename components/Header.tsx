274f8723155cfce714b8749c5cc34fa2830d2b9c
snowye <10700876+snowye@user.noreply.gitee.com>
2024-01-13 03:01:09 +0800

    "添加height"

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between gap-3">
-            <Image src="/static/images/snow-globe.gif" alt="" width={82} />
+            <Image src="/static/images/snow-globe.gif" alt="" width={82} height={82} />
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block leading-6">
                 {siteMetadata.headerTitle}
