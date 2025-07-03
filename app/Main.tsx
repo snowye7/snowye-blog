f522ef50320183ff0423be4c7111e1c083ecc447
snowye7 <601994226@qq.com>
2025-07-03 15:16:07 +0800

    ✨feature: crossOrigin

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,9 +1,8 @@
+import Hero from '@/components/Hero'
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
-import Hero from '@/components/Hero'
 
 const MAX_DISPLAY = 5
 
